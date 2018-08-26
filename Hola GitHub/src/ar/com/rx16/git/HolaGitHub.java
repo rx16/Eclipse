628886725a01d62f1c73c40efac1bee6b6ee2ac0
rx16 <42719411+rx16@users.noreply.github.com>
2018-08-26 15:24:31 -0300

    Add files via upload
    Project Test Integration Eclipse and GitHub for upload

---
+++ Hola GitHub/src/ar/com/rx16/git/HolaGitHub.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ar.com.rx16.git;
+
+/**
+ * @author rx16
+ *
+ */
+public class HolaGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Hola GitHub/src/ar/com/rx16/git/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author rx16
+ *
+ */
+package ar.com.rx16.git;
